4dde4674ef1d94993830eca1d94bb74630c63591
Jacob Shin <jacobshin313@gmail.com>
2021-12-07 00:22:29 -0500

    Add day 07

---
+++ 07/main1.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int get_cost(int i, vector<int> & crab_pos) {
+    int fuel = 0;
+    for (auto pos: crab_pos) {
+        fuel += abs(pos - i);
+    }
+
+    /* cout << fuel << endl; */
+    return fuel;
+}
+
+int main(int argc, char **argv)
+{
+    freopen("input2.txt", "r", stdin);
+
+    string s;
+    cin >> s;
+    std::stringstream ss(s);
+
+    vector<int> crab_pos;
+
+    int max_pos = 0;
+    int min_pos = INT_MAX;
+
+    for (int i; ss >> i;) {
+        max_pos = max(max_pos, i);
+        min_pos = min(min_pos, i);
+
+        crab_pos.push_back(i);    
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+
+
+    int min_cost = INT_MAX;
+    for (int i = min_pos; i <= max_pos; i++) {
+        min_cost = min(get_cost(i, crab_pos), min_cost);
+    }
+
+    cout << min_cost << endl;
+}
