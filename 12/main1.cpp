83fc48d4063b7d6df6c0b06e0026fcec417c9202
Jacob Shin <jacobshin313@gmail.com>
2021-12-12 15:37:29 -0500

    Make code cleaner for part 1

--- 12/main1.cpp
+++ 12/main1.cpp
@@ -14,52 +14,41 @@ bool is_lowercase(string node) {
     return true;
 }
 
-int find_small_cave_paths(int &total, vector<string> &paths, bool has_small, string start_node, unordered_map<string, vector<string>> &adj_list, unordered_map<string, bool> &visited) {
-    /* cout << start_node << " "; */
-    visited[start_node] = true;
-    paths.push_back(start_node);
+void print_path(const vector<string> &visited) {
+    for (auto node: visited) {
+        cout << node << " ";
+    }
+    cout << endl;
+}
+
+int find_paths(string start_node, unordered_map<string, vector<string>> &adj_list, vector<string> &visited) {
     int count = 0;
     for (auto node: adj_list[start_node]) {
+        visited.push_back(node);
         if (node == "end") {
-            if (has_small) {
-                /* cout << " <-- count++"; */
-                count++;
-            }
-            total++;
-
-            paths.push_back(node);
-            for (auto node: paths) {
-                cout << node << " ";
-            }
-            cout << endl;
-            paths.pop_back();
+            count++;
+            print_path(visited);
         } else {
-            if (!(is_lowercase(node) && visited[node])) {
-                count += find_small_cave_paths(total, paths, has_small, node, adj_list, visited);
-                if (is_lowercase(node)) {
-                    has_small = true;
-                }
+            if (!is_lowercase(node) || std::count(visited.begin(), visited.end(), node) <= 1) {
+                count += find_paths(node, adj_list, visited);
             }
         }
+        visited.pop_back();
     }
-    paths.pop_back();
-    visited[start_node] = false;
 
     return count;
 }
 
-int count_small_cave_paths(unordered_map<string, bool> &visited, unordered_map<string, vector<string>> &adj_list) {
-    vector<string> paths;
-    int total = 0;
-    int count = find_small_cave_paths(total, paths, false, "start", adj_list, visited);
-    cout << total << endl;
-    return count;
+int count_paths(unordered_map<string, vector<string>> &adj_list) {
+    vector<string> visited;
+
+    visited.push_back("start");
+    return find_paths("start", adj_list, visited);
 }
 
 int main(int argc, char **argv)
 {
     freopen("input2.txt", "r", stdin);
-    unordered_map<string, bool> visited;
 
     string s;
     unordered_map<string, vector<string>> adj_list;
@@ -76,38 +65,31 @@ int main(int argc, char **argv)
             second_node += s[i];
         }
 
-        if (adj_list.find(first_node) == adj_list.end()) {
-            vector<string> adj_nodes;
-            adj_list[first_node] = adj_nodes;
-            visited[first_node] = false;
-        }
-
-        if (adj_list.find(second_node) == adj_list.end()) {
-            vector<string> adj_nodes;
-            adj_list[second_node] = adj_nodes;
-            visited[second_node] = false;
-        }
-
         if (first_node != "end" && second_node != "start") {
+            if (adj_list.find(first_node) == adj_list.end()) {
+                vector<string> v;
+                adj_list[first_node] = v;
+            }
             adj_list[first_node].push_back(second_node);
         }
 
-        if (second_node != "end" && first_node != "start") {
+        if (first_node != "start" && second_node != "end") {
+            if (adj_list.find(second_node) == adj_list.end()) {
+                vector<string> v;
+                adj_list[second_node] = v;
+            }
             adj_list[second_node].push_back(first_node);
-
         }
-
     }
 
     for (auto it: adj_list) {
         cout << it.first << ": ";
         for (auto elem: it.second) {
-            cout << elem << ",";
+            cout << elem << " ";
         }
         cout << endl;
     }
 
-    count_small_cave_paths(visited, adj_list);
-
+    cout << count_paths(adj_list) << endl;
 
 }
